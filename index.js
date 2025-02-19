2a30ebb5eeef3dc28a81d7cd027bf34f1eb42f66
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-18 22:08:36 -0300

    Exercícios de Higher-Order Functions 18/02/24

--- index.js
+++ index.js
@@ -13,4 +13,30 @@ const mensagemBoasVindas = (nome) => {
     return `Seja bem-vindo, ${nome}!`;
 };
 console.log(mensagemBoasVindas("Carlos"));
-console.log(mensagemBoasVindas("Ana"));    
+console.log(mensagemBoasVindas("Ana"));
+
+//ex 4 -> higher-order function personalizada
+function executarOperacao(a, b, operacao) {
+    return operacao(a, b);
+}
+console.log(executarOperacao(6, 3, (a, b) => a - b));
+console.log(executarOperacao(4, 5, (a, b) => a * b));
+console.log(executarOperacao(10, 2, (a, b) => a + b));
+console.log(executarOperacao(8, 4, (a, b) => a / b));
+
+//ex 5 -> map() - transformação de array
+const precos = [100, 200, 300, 400];
+const precosComDesconto = precos.map(preco => preco * 0.9);
+console.log(precosComDesconto);
+
+//ex 6 -> filter() - filtrando valores em um array
+const idades = [12, 16, 18, 26, 14, 29, 15];
+const menoresDeIdade = idades.filter(idade => idade < 18);
+console.log(menoresDeIdade);
+
+//ex 7 -> reduce() - somando valores de um array
+const numeros = [2, 3, 4, 5];
+const produto = numeros.reduce((total, num) => total * num, 1);
+console.log(produto);
+
+
